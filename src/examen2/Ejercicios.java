5dc3d02a2df341f26951a6a05ba7e65a5292c951
turupawn <ahmed.hn.43@gmail.com>
2018-06-30 11:09:30 -0600

    first commit

---
+++ src/examen2/Ejercicios.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author turupawn
+ */
+public class Ejercicios {
+    static void guardar(String nombre_archivo, ArrayList<Producto> productos)
+    {
+    }
+    
+    static ArrayList<Producto> abrir(String nombre_archivo)
+    {
+        return null;
+    }
+}
